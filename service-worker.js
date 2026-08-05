9a13caa8817c8874dc323ff88ba652f9e989f3fc
gdefer4-dot <gdefer4@gmail.com>
2026-08-05 14:58:04 +0200

    V45.7.18 - Mise à jour automatique du planning

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const VERSION = "V45.7.17";
-const CACHE_NAME = "planning-dojo-club-v45-7-17";
+const VERSION = "V45.7.18";
+const CACHE_NAME = "planning-dojo-club-v45-7-18";
 const APP_SHELL = [
   "./",
   "./index.html",
