087e3b4168479ca7140476c901b92527a7033627
gdefer4-dot <gdefer4@gmail.com>
2026-08-07 14:18:49 +0200

    V49.0.26 - Mise à jour automatique du planning

--- sw-v481.js
+++ sw-v481.js
@@ -1,5 +1,5 @@
-const VERSION = "V49.0.25";
-const CACHE_NAME = "planning-dojo-club-v49-0-25";
+const VERSION = "V49.0.26";
+const CACHE_NAME = "planning-dojo-club-v49-0-26";
 const APP_SHELL = [
   "./",
   "./index.html",
